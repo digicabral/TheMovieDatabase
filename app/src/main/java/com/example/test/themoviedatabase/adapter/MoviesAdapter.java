371e77e08a654670e2fc9dc8cedc4622c2530282
digicabral <digicabral@gmail.com>
2017-11-21 17:22:31 -0200

    Java Classes

--- app/src/main/java/com/example/test/themoviedatabase/adapter/MoviesAdapter.java
+++ app/src/main/java/com/example/test/themoviedatabase/adapter/MoviesAdapter.java
@@ -2,7 +2,14 @@ package com.example.test.themoviedatabase.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.test.themoviedatabase.R;
 import com.example.test.themoviedatabase.model.Movie;
 
 import java.util.List;
@@ -22,5 +29,37 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHold
     }
 
     @Override
-    public MoviesAdapter.MyViewHolder onCreateViewHolder
+    public MoviesAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.movie_card, viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MoviesAdapter.MyViewHolder viewHolder, int i){
+        viewHolder.title.setText(movieList.get(i).getOriginalTitle());
+        String vote = Double.toString(movieList.get(i).getVoteAverage());
+        viewHolder.userrating.setText(vote);
+
+        Glide.with(mContext)
+                .load(movieList.get(i).getPosterPath())
+                .placeholder(R.drawable.load);
+    }
+
+    @Override
+    public int getItemCount(){
+        return movieList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView title, userrating;
+        public ImageView thumbnail;
+
+        public MyViewHolder(View view){
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            userrating = (TextView)view.findViewById(R.id.userRating);
+            thumbnail = (ImageView)view.findViewById(R.id.thumbnail);
+        }
+    }
 }
